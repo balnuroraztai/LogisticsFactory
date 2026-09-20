3a584406375baebf69876331020eda18944f9be6
balnuroraztai <balnuroraztai@gmail.com>
2026-09-20 17:04:21 +0500

    Add abstract Logistics creator

---
+++ src/factorymethod/Logistics.java
@@ -0,0 +1,11 @@
+package factorymethod;
+
+public abstract class Logistics {
+
+    public abstract Transport createTransport();
+
+    public void planDelivery() {
+        Transport transport = createTransport();
+        transport.deliver();
+    }
+}
