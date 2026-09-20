e8f268a554a4dfac004a1ff09f065bb9792aed99
balnuroraztai <balnuroraztai@gmail.com>
2026-09-20 16:57:56 +0500

    Add Ship transport implementation

---
+++ src/factorymethod/Ship.java
@@ -0,0 +1,9 @@
+package factorymethod;
+
+public class Ship implements Transport {
+
+    @Override
+    public void deliver() {
+        System.out.println("Delivering cargo by sea in a ship.");
+    }
+}
