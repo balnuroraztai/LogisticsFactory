27fe60da6f067f2b8a34540abbf2c6f565dab526
balnuroraztai <balnuroraztai@gmail.com>
2026-09-20 17:20:47 +0500

    Add Main class to demonstrate Factory Method

---
+++ src/factorymethod/Main.java
@@ -0,0 +1,13 @@
+package factorymethod;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Logistics roadLogistics = new RoadLogistics();
+        roadLogistics.planDelivery();
+
+        Logistics seaLogistics = new SeaLogistics();
+        seaLogistics.planDelivery();
+    }
+}
