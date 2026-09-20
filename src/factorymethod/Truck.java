4823c8961b4e0d54abd86a9c45b4c49e7404d928
balnuroraztai <balnuroraztai@gmail.com>
2026-09-20 16:54:14 +0500

    Add Transport interface and Truck implementation

---
+++ src/factorymethod/Truck.java
@@ -0,0 +1,9 @@
+package factorymethod;
+
+public class Truck implements Transport {
+
+    @Override
+    public void deliver() {
+        System.out.println("Delivering cargo by road in a truck.");
+    }
+}
